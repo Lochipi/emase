350acaadc7909291f33c60ce09d226a314fce399
Cornelius Emase <corneliuslochipi@gmail.com>
2023-10-27 19:04:53 +0300

    updated smooth scrolling

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ const ubuntu_Mono = Ubuntu({ subsets: ["latin"], weight: ["400", "700"] });
 
 export const metadata: Metadata = {
   title: "Cornelius | Software Engineer",
-  description: "Portfolio of Cornelius",
+  description: "Cornelius' Portfolio",
 };
 
 export default function RootLayout({
