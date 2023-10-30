626ac517c26fadda98e352c5193847321587bc46
Cornelius Emase <corneliuslochipi@gmail.com>
2023-10-30 11:43:00 +0300

    updated latest blog data

--- assets/blogsData.tsx
+++ assets/blogsData.tsx
@@ -1,6 +1,15 @@
 const blogsData = [
   {
     id: 1,
+    title: "How technical writing is growing as a career.",
+    desc: "Is technical writing a career by itself? can I build a career by writing?",
+    time: "4 min",
+    date: "25th October,2023",
+    link: "https://medium.com/@corneliuslochipi/how-technical-writing-is-growing-as-a-career-29263c6924f0",
+  },
+  ,
+  {
+    id: 2,
     title: "Why I got rejected as a GDSC Lead😓",
     desc: "Why do I lose a chance to lead a community and what are some of the best practices to do when applying for a lead?",
     time: "5 min",
@@ -8,7 +17,7 @@ const blogsData = [
     link: "https://medium.com/@corneliuslochipi/why-i-got-rejected-as-a-gdsc-lead-b7fc3e4e2ed7",
   },
   {
-    id: 2,
+    id: 3,
     title: "The Competitive Edge: Why Staying Up to Date Matters",
     desc: "Staying up-to-date with the latest trends and developments in your field is an important aspect of being a successful developer. While it…",
     time: "4 min",
@@ -16,7 +25,7 @@ const blogsData = [
     link: "https://corneliusemase.hashnode.dev/a-sneak-peak-into-apis-in-javascript-applications",
   },
   {
-    id: 3,
+    id: 4,
     title: "Technical Writing 101: Technical Ultimate Guide",
     desc: "Getting started with technical writing ultimate guide. All you need to know about getting started as a Writer!",
     time: "5 min",
@@ -24,21 +33,13 @@ const blogsData = [
     link: "https://medium.com/@corneliuslochipi/technical-writing-101-technical-ultimate-guide-dbfd09909c51",
   },
   {
-    id: 4,
+    id: 5,
     title:
       "Best Practices for Efficiently Rendering Data from Arrays in React JS.",
     desc: "Expert-Recommended Strategies for Rendering and Visualizing Data from Arrays in ReactJS",
     time: "3 min",
     date: "30st May,2023",
     link: "https://corneliusemase.hashnode.dev/best-practices-for-efficiently-rendering-data-from-arrays-in-react-js",
-  },
-  {
-    id: 5,
-    title: "A Sneak Peak into APIs in JavaScript Applications.",
-    desc: "Working with APIs in JavaScript - explaining how to incorporate APIs into JavaScript applications to retrieve and manipulate data from external source",
-    time: "4 min",
-    date: "4th August,2023",
-    link: "https://corneliusemase.hashnode.dev/a-sneak-peak-into-apis-in-javascript-applications",
   },
 ];
 
