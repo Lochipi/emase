d29621c88b71a98b34345794e28f3455f4e78638
Cornelius Emase <corneliuslochipi@gmail.com>
2024-01-08 15:37:14 +0300

    updated icon and nodemailer

--- app/api/send/route.tsx
+++ app/api/send/route.tsx
@@ -1,20 +1,11 @@
-import { EmailTemplate } from "@/app/components/email-template";
-import { Resend } from "resend";
+const nodemailer = require("nodemailer");
 
-const resend = new Resend(process.env.RESEND_API_KEY);
-
-export async function POST() {
-  try {
-    const data = await resend.emails.send({
-      from: "Acme <onboarding@resend.dev>",
-      to: ["corneliuslochipi@gmail.com"],
-      subject: "Hello world",
-      text: "Hello John, welcome to our platform!",
-      react: EmailTemplate({ firstName: "John" }),
-    });
-
-    return Response.json(data);
-  } catch (error) {
-    return Response.json({ error });
-  }
-}
+nodemailer.createTransport({
+  host: "smtp.example.com",
+  port: 587,
+  secure: true, // upgrade later with STARTTLS
+  auth: {
+    user: "username",
+    pass: "password",
+  },
+});
